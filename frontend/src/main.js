2eef94dd6571cb5c51411b2f229897e4929e4822
devedu5 <devedu5@ecount.kr>
2024-07-31 19:28:51 +0900

    프론트서버 통신 파일 생성

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  alert('hi')
+  const fetchTest=async ()=>{
+    const BASE_URL='http://172.29.12.170:5050';
+    const response=await fetch(`${BASE_URL}/`)
+    console.log(response)
+    if(response.ok) {
+      // alert(await((await (response.json())).name))
+      
+      // 테스트용
+      console.log( await (response.json()))
+    }
+  }
+
+  fetchTest()
   });
 
