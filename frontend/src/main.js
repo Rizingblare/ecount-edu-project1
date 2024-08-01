d16b4d5fbdb3165ea10a69b55f5792c377220e5b
devedu5 <devedu5@ecount.kr>
2024-08-01 13:01:56 +0900

    로컬 ip주소 변경

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fetchTest = async () => {
-    const BASE_URL = "http://172.29.12.151:5050"; // 본인 ip로 변경하여 사용
+    const BASE_URL = "http://172.29.12.170:5050"; // 본인 ip로 변경하여 사용
     const response = await fetch(`${BASE_URL}/`);
     console.log(response);
     if (response.ok) {
