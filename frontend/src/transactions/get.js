3541866b2938fcb186428dd10eb0b55e99f1511e
devedu5 <devedu5@ecount.kr>
2024-08-01 17:44:33 +0900

    프론트엔드 get api 생성

--- frontend/src/transactions/get.js
+++ frontend/src/transactions/get.js
@@ -1,9 +1,24 @@
 export const getTransactionData = async () => {
-  const startYear = document.querySelector(".startYear");
-  const startMonth = document.querySelector(".startMonth");
-  const startDay = document.querySelector(".startDay");
+  const searchButton = document.querySelector(".search");
 
-  const endYear = document.querySelector(".endYear");
-  const endMonth = document.querySelector(".endMonth");
-  const endDay = document.querySelector(".endDay");
+  searchButton.addEventListener("click", async () => {
+    const startYear = document.querySelector(".startYear").value;
+    const startMonth = document.querySelector(".startMonth").value;
+    const startDay = document.querySelector(".startDay").value;
+    const endYear = document.querySelector(".endYear").value;
+    const endMonth = document.querySelector(".endMonth").value;
+    const endDay = document.querySelector(".endDay").value;
+
+    const startDate = [startYear, startMonth, startDay].join("-");
+    const endDate = [endYear, endMonth, endDay].join("-");
+
+    const response = await fetch(
+      `http://172.29.12.170:5050/transactions?startDate=${startDate}&endDate=${endDate}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const jsonDate = await response.json();
+  });
 };
