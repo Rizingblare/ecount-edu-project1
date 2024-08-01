1d3d0b69df698672a868c8b2cda1081d4531c7f1
Junbong <becoding96@gmail.com>
2024-08-01 10:06:41 +0900

    post

--- backend/src/test.js
+++ backend/src/test.js
@@ -1,59 +1,56 @@
 // // 프론트 - 백 DB데이터 통신 테스트 파일
 const express = require("express");
 const app = express();
-const PORT=5050
-const HOST='172.29.12.170'
-
+const PORT = 5050;
+const HOST = "172.29.12.151";
 
 // CONFIG
 const { Client } = require("pg");
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://172.29.12.170:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "http://172.29.12.151:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-
+// JSON 파싱 미들웨어
+app.use(express.json());
 
 const client = new Client({
   user: "postgres",
-  host: "127.0.0.1", // TODO: 특정 IP주소 명시 옵션 설정 
-  database: "project2",
-  password: "minh0518",
+  host: "127.0.0.1", // TODO: 특정 IP주소 명시 옵션 설정
+  database: "project1",
+  password: "1234",
   port: 5432,
 });
 
 client.connect();
 
-
-
 // CONTROLLOER
 app.get("/", (req, res) => {
-  
-    client.query("SELECT * FROM transactions", (err, queryRes) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
-      } else {
-        console.log(queryRes.rows)
-        res.json(queryRes.rows);
-      }
-    });
-
-    
+  client.query("SELECT * FROM transactions", (err, queryRes) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      console.log(queryRes.rows);
+      res.json(queryRes.rows);
+    }
   });
-
-
+});
 
 app.post("/transactions", async (req, res) => {
-  const { date, type, amount, description } = req.body;
+  const { type, date, amount, description } = req.body;
+  console.log(type);
 
   try {
     const query =
       "INSERT INTO Transactions (date, type, amount, description) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [date, type, amount, description];
 
-    const result = await pool.query(query, values);
+    const result = await client.query(query, values);
 
     res.status(201).json(result.rows[0]);
   } catch (error) {
@@ -62,8 +59,6 @@ app.post("/transactions", async (req, res) => {
   }
 });
 
-
-
 app.listen(PORT, HOST, () => {
-    console.log(`API 서버가 http://0.0.0.0:${PORT} 에서 실행 중입니다.`);
+  console.log(`API 서버가 http://0.0.0.0:${PORT} 에서 실행 중입니다.`);
 });
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const fetchTest=async ()=>{
-    const BASE_URL='http://172.29.12.170:5050';
-    const response=await fetch(`${BASE_URL}/`)
-    console.log(response)
-    if(response.ok) {
+document.addEventListener("DOMContentLoaded", () => {
+  const fetchTest = async () => {
+    const BASE_URL = "http://172.29.12.151:5050"; // 본인 ip로 변경하여 사용
+    const response = await fetch(`${BASE_URL}/`);
+    console.log(response);
+    if (response.ok) {
       // alert(await((await (response.json())).name))
-      
+
       // 테스트용
-      console.log( await (response.json()))
+      console.log(await response.json());
     }
-  }
+  };
 
-  fetchTest()
-  });
-
+  fetchTest();
+});
